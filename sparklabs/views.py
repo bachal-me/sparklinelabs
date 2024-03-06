435015cf53761141d83470af6a57306c123bba6a
Muhammad Bachal <mbachalabro1@gmail.com>
2024-03-07 00:05:51 +0500

    Database added

--- sparklabs/views.py
+++ sparklabs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-# from . models import Contact, Project
+from . models import Contact, Project
 # Create your views here.
 def home(request):
     return render(request, 'sparklabs/index.html')
@@ -11,17 +11,16 @@ def services(request):
     return render(request, 'sparklabs/services.html')
 
 def portfolio(request):
-    # projects = Project.objects.all()
-    # context={'pr':projects}
-    return render(request, 'sparklabs/portfolio.html')
+    projects = Project.objects.all()
+    return render(request, 'sparklabs/portfolio.html', context={'pr':projects})
 
 def contact(request):
-    # if request.method=="POST":
-        # name = request.POST.get('name', '')
-        # email = request.POST.get('email', '')
-        # phone = request.POST.get('phone', '')
-        # sub = request.POST.get('email_sub', '')
-        # msg = request.POST.get('message', '')
-        # contact = Contact(name=name, email=email, phone=phone, email_sub=sub, message=msg)
-        # contact.save()
+    if request.method=="POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        sub = request.POST.get('email_sub', '')
+        msg = request.POST.get('message', '')
+        contact = Contact(name=name, email=email, phone=phone, email_sub=sub, message=msg)
+        contact.save()
     return render(request, 'sparklabs/contact.html')
--- sparklinelabs/settings.py
+++ sparklinelabs/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'sparklinelabs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'sparklinelabs',
+        'USER':'bachal',
+        'PASSWORD':'bachal',
+    }
+}
 
 
 # Password validation
@@ -116,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Managing Media
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
