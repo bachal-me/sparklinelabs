532e2a58446ad3990c9032f79bdccd901901c22e
Muhammad Bachal <mbachalabro1@gmail.com>
2024-03-07 00:23:36 +0500

    Static_files changed to media

--- sparklinelabs/settings.py
+++ sparklinelabs/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
